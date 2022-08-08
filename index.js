74b361790c67b68805ddce638f56ef5a9b23c18c
Meet Vora <47553376+vmeet24@users.noreply.github.com>
2022-08-08 02:36:44 -0400

    Add files via upload
    intial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+(function () {
+    new Typed('#console1', {
+        strings: [' echo hostname<br>'],
+        typeSpeed: 100,
+        showCursor: false
+    });
+    setTimeout(() => {
+        var x = document.getElementsByClassName("s1")[0];
+        console.log(x);
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }, 2200);   
+    new Typed('#console2', {
+        strings: ['^2320 echo education<br>'],
+        typeSpeed: 100,
+        showCursor: false
+    });
+    setTimeout(() => {
+        var x = document.getElementsByClassName("s2")[0];
+        console.log(x);
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }, 4800);
+    new Typed('#console3', {
+        strings: ['^5000 dir<br>'],
+        typeSpeed: 100,
+        showCursor: false
+    });
+    setTimeout(() => {
+        var x = document.getElementsByClassName("s3")[0];
+        console.log(x);
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }, 6000);
+    new Typed('#console4', {
+        strings: ['^6600 intro.txt<br>'],
+        typeSpeed: 100,
+        showCursor: false
+    });
+    setTimeout(() => {
+        var x = document.getElementsByClassName("s4")[0];
+        console.log(x);
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }, 8500);
+})();
